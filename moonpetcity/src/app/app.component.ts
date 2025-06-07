34439939f0b84a2cf57d9ed803ecff392b60f4c7
wilmer_sanchez <wilmerjaviers@yahoo.com>
2025-06-07 09:25:23 -0600

    componente acerca de

--- moonpetcity/src/app/app.component.ts
+++ moonpetcity/src/app/app.component.ts
@@ -2,11 +2,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, InicioComponent],
+  imports: [CommonModule, InicioComponent, AcercaDeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
