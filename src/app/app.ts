b925eaa9c4e96de360d7dd9112445747b6157ace
Jenny Ferrufino <jfer.fullstack@gmail.com>
2025-06-07 07:47:58 -0600

    Primer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./navbar/navbar";
+import { Home } from "./home/home";
+import { AboutUs } from './about-us/about-us';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Home, AboutUs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
